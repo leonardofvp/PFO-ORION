0defa6685fd35d6bbad0a0838135d5fcf045a495
Leandro Ferrero <learferrero@gmail.com>
2026-04-16 19:59:46 -0300

    Eliminar clases y modelos no necesarios para la primera entrega

--- models/Pago.js
+++
@@ -1,9 +0,0 @@
-class Pago {
-    constructor(id, monto, metodoPago, fecha, idGasto) {
-        this.id = id;
-        this.monto = monto;
-        this.metodoPago = metodoPago;
-        this.fecha = fecha;
-        thid.idGasto = idGasto;
-    }
-}
--- models/Usuario.js
+++
@@ -1,9 +0,0 @@
-class Usuario {
-    constructor(id, nombre, rol) {
-        this.id = id;
-        this.nombre = nombre;
-        this.rol = rol;
-    }
-}
-
-module.exports = Usuario;
