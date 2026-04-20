2abc93fca447c16950dd2ba9308fb921f46c52b1
Leandro Ferrero <learferrero@gmail.com>
2026-04-20 18:00:24 -0300

    Agregadas funciones para buscar gasto y obra por id, para usarlas en el CRUD

--- controllers/gastosController.js
+++ controllers/gastosController.js
@@ -1,1 +1,11 @@
 // Controller de gastos, aca van las funciones para el CRUD de los gastos
+
+/**
+ * Busca un gasto específico en el archivo JSON por su ID.
+ * * @param {number|string} id - El identificador único del gasto a buscar.
+ * @returns {Object|undefined} El objeto del gasto si se encuentra, o undefined si no existe.
+ */
+const buscarGastoPorId = (id) => {
+    const gastos = leerArchivo("gastos.json");
+    return obras.find(g => g.id == id);
+};
--- controllers/obrasController.js
+++ controllers/obrasController.js
@@ -1,1 +1,11 @@
-// Controller de obras, aca van las funciones para el CRUD de las obras
+// Controller de obras, aca van las funciones para el CRUD de las obrasgit
+
+/**
+ * Busca una obra específica en el archivo JSON por su ID.
+ * * @param {number|string} id - El identificador único de la obra a buscar.
+ * @returns {Object|undefined} El objeto de la obra si se encuentra, o undefined si no existe.
+ */
+const buscarObraPorId = (id) => {
+    const obras = leerArchivo("obras.json");
+    return obras.find(o => o.id == id);
+};
