76aef062c176c82a311a9d89293e04b0a220feb3
Leandro Ferrero <learferrero@gmail.com>
2026-06-24 00:53:26 -0300

    Instalado mongodb-memory-sever para pruebas y refactorizada la conexion a la db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-export const conectarDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Base de datos conectada");
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+const mongoServer = new MongoMemoryServer();
+
+const getUri = async () => {
+  if (process.env.NODE_ENV === "test") {
+    await mongoServer.start();
+    return mongoServer.getUri();
+  }
+  return process.env.MONGO_URI;
+};
+
+const conectarDB = async (uri) => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Base de datos conectada");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export { getUri, conectarDB };
--- index.js
+++ index.js
@@ -2,35 +2,37 @@ import dotenv from "dotenv";
 import app from "./app.js";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { conectarDB } from "./config/db.js";
+import { getUri, conectarDB } from "./config/db.js";
 import configurarChat from "./sockets/chatInstitucional.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+const uri = getUri();
+
 configurarChat(io);
 
-
 try {
-  await conectarDB();
+  await conectarDB(uri);
   console.log("Conexión a la base de datos establecida.");
+
+  const server = httpServer.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  });
+
+  server.on("error", (err) => {
+    if (err.code === "EADDRINUSE") {
+      console.error(
+        `El puerto ${PORT} ya está siendo usado por otro programa.`,
+      );
+    } else {
+      console.error("Error inesperado en el servidor:", err);
+    }
+  });
 } catch (error) {
   console.error("Error al iniciar la base de datos:", error);
 }
-
-const server = httpServer.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
-
-server.on("error", (err) => {
-  if (err.code === "EADDRINUSE") {
-    console.error(`El puerto ${PORT} ya está siendo usado por otro programa.`);
-  } else {
-    console.error("Error inesperado en el servidor:", err);
-  }
-});
